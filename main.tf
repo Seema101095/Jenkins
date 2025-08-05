214bdb5862c6c3dc2d7ab71ea1e22f52a54eb27b
Seema101095 <seemakumari10oct@gmail.com>
2025-08-05 17:55:00 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    region = "us-east-2"
+  
+}
+resource "aws_instance" "ec2seema" {
+    ami = var.ami
+    instance_type = var.instance_type
+    tags={
+        Name = "ec2"
+    }
+    availability_zone = "us-east-2c"
+
+}
